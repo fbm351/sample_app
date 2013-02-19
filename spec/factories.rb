539dff77c834d65bda1024fa281918bbd059f17a
Fred Myers <fred@fred-myers.com>
2013-02-19 10:32:30 -0500

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+    factory :user do
+        name "Fred Myers"
+        email "fred@fred-myers.com"
+        password "startstart"
+        password_confirmation "startstart"
+    end
+end
